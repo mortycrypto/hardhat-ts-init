f0fac75a4e3d6b4c70e961bfb294485caa6e3f56
Maximiliano <fergthh@gmail.com>
2021-10-22 20:15:23 -0300

    add gas reporter, coverage and tracer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,9 @@
 import { task } from "hardhat/config";
 import '@typechain/hardhat'
 import "@nomiclabs/hardhat-waffle";
+import "hardhat-tracer";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -19,13 +22,17 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 export default {
+	gasReporter: {
+		enabled: false,
+	},
+	skipFiles: [], // Coverage
 	solidity: {
 		compilers: [
 			{ version: "0.4.18" },
 			{ version: "0.4.22" },
 			{ version: "0.6.12" },
 			{ version: "0.7.6" },
-			{ version: "0.8.7" },
+			{ version: "0.8.4" },
 		],
 	},
 };
